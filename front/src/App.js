4cb41844fd167d90b268cb6f1cba8ef64c4e6d02
pache_pot <heedoo0303@naver.com>
2024-07-08 16:51:58 +0900

    First Commit

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './LoginForm';
+import LoginForm from "./LoginForm";
+
+function App() {
+    return (
+        <div className="App">
+            <LoginForm></LoginForm>
+        </div>
+    );
+}
+
+export default App;
