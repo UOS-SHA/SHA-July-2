b3029b4c8e745bddce0378b7d07f8af82ef5451b
pachepot <heedoo0303@naver.com>
2024-07-15 16:25:12 +0900

    Add Home, Sign up front-end. Renew login form

---
+++ client/src/components/Home.js
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
+
+const { Header } = Layout;
+
+function Home() {
+    return (
+        <Layout>
+            <Header style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', backgroundColor: "#ffffff"}}>
+                <Menu mode="horizontal" style={{ border: 'none' }}>
+                    <Menu.Item key="login">
+                        <Link to="/login">Login</Link>
+                    </Menu.Item>
+                    <Menu.Item key="signup">
+                        <Link to="/signup">Signup</Link>
+                    </Menu.Item>
+                    <Menu.Item key="user">
+                        {/* Replace 'John Doe' with the current user's name */}
+                        Test name
+                    </Menu.Item>
+                </Menu>
+            </Header>
+            {/* Your other content goes here */}
+        </Layout>
+    );
+}
+
+export default Home;
