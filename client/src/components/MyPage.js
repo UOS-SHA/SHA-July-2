07c4105bf1277da96612278eb3066b35093cff97
minnnjuuu <kmj020411@uos.ac.kr>
2024-08-07 17:28:29 +0900

    point 추가

--- client/src/components/MyPage.js
+++ client/src/components/MyPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useCookies } from 'react-cookie';
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
 import { Layout, Card, Typography, Spin, Button } from 'antd';
 import pasino from "./Pasino.png";
@@ -9,7 +9,7 @@ const { Header, Content } = Layout;
 const { Title, Text } = Typography;
 
 const MyPage = () => {
-    const [userInfo, setUserInfo] = useState(null);
+    const [userInfo, setUserInfo] = useState({});
     const [cookies, setCookie, removeCookie] = useCookies(['token']);
     const navigate = useNavigate();
 
@@ -41,14 +41,14 @@ const MyPage = () => {
     return (
         <Layout style={{ minHeight: '100vh' }}>
             <Header style={{ backgroundColor: '#ffffff', zIndex: 1 }}>
-                <Link to="/" style={{width: '100px', display: 'block'}}>
-                    <div style={{display: 'flex', alignItems: 'center'}}>
-                        <img src={pasino} style={{width: "250px", marginTop: "16px"}}/>
+                <Link to="/" style={{ width: '100px', display: 'block' }}>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <img src={pasino} style={{ width: "250px", marginTop: "16px" }} />
                     </div>
                 </Link>
             </Header>
-            <Content style={{display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '50px', backgroundColor: '#ffffff',}}>
-                <Card style={{width: '400px', textAlign: 'center' }}>
+            <Content style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '50px', backgroundColor: '#ffffff', }}>
+                <Card style={{ width: '400px', textAlign: 'center' }}>
                     <Title level={2}>My Page</Title>
                     {userInfo ? (
                         <div>
@@ -60,7 +60,7 @@ const MyPage = () => {
                     )}
                     <div style={{ marginTop: '20px' }}>
                         <Text>Point:</Text>
-                        <pre style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-all' }}>1232323231</pre>
+                        <p>{userInfo.point !== undefined ? userInfo.point : 'N/A'}</p>
                     </div>
                     <Button type="primary" onClick={handleLogout} style={{ marginTop: '20px' }}>
                         Logout
